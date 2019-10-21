0071647e9728c639a384273a1860ccfdcc56f3fc
Broccolito <wanjungu@yahoo.com>
2019-10-20 17:00:00 -0700

    auto sync

--- app.R
+++ app.R
@@ -26,26 +26,33 @@ if(!require("shinyjs")){
   install.packages("shinyjs")
   library("shinyjs")
 }
+if(!require("shinyAce")){
+  install.packages("shinyAce")
+  library("shinyAce")
+}
 
 headerUI = dropdownMenu(messageItem(from = "Admin",
                                     message = "Welcome to Physio HBR!"),
                         type = "messages", badgeStatus = "primary")
 
 sidebarUI = div(
-  br(),
-  downloadBttn(outputId = "sample_data", label = "Download Sample Data", style = "simple"),
-  fileInput(inputId = "data_file", label = "Upload Data File", 
-            multiple = FALSE, accept = c(".csv", ".xlsx")),
-  sliderInput("factor_selection", label = "Factor Range", min = 1, 
-              max = 100, value = c(1, 2)),
-  sliderInput("numeric_selection", label = "Numeric Range", min = 1, 
-              max = 100, value = c(1, 2)),
-  h4(HTML("<hr>&nbsp&nbsp"), "Analysis"),
   sidebarMenu(
+    br(),
+    downloadBttn(outputId = "sample_data", label = "Download Sample Data", style = "simple"),
+    h4(HTML("<hr>&nbsp&nbsp"), "Upload Data"),
+    menuItem("Data Preparation",
+             br(),
+             fileInput(inputId = "data_file", label = "Upload Data File", 
+                       multiple = FALSE, accept = c(".csv", ".xlsx")),
+             sliderInput("factor_selection", label = "Factor Range", min = 1, 
+                         max = 100, value = c(1, 2)),
+             sliderInput("numeric_selection", label = "Numeric Range", min = 1, 
+                         max = 100, value = c(1, 2))),
+    h4(HTML("<hr>&nbsp&nbsp"), "Analysis"),
     menuItem("Covariance Heatmap", tabName = "heatmap_menu"),
     menuItem("Covariance Diff Heatmap", tabName = "diff_heatmap_menu"),
     menuItem("Boxplot", tabName = "boxplot_menu"),
-    menuItem("Regression Plot", "regression_menu")
+    menuItem("Regression Plot", tabName = "regression_menu")
   ),
   h4(HTML("<hr>&nbsp&nbsp"), "Advanced"),
   sidebarMenu(
@@ -59,35 +66,23 @@ sidebarUI = div(
 )
 
 bodyUI = div(
-  uiOutput("header_title"),
-  tableOutput("data_summary"), 
-  verbatimTextOutput("factor_data_names"),
-  verbatimTextOutput("numeric_data_names"),
-  uiOutput("show_hide_summary"),
-  br(),
-  dataTableOutput("data_view"),
+  box(width = 12,collapsible = TRUE, collapsed = FALSE, solidHeader = TRUE,
+      uiOutput("header_title"),
+      tableOutput("data_summary"),
+      verbatimTextOutput("factor_data_names"),
+      verbatimTextOutput("numeric_data_names"),
+      uiOutput("show_hide_summary"),
+      br(),
+      dataTableOutput("data_view")
+  ),
   tabItems(
-    tabItem("heatmap_menu",
-            div(p("Dashboard tab content"))
-    ),
-    tabItem("diff_heatmap_menu",
-            "Widgets tab content"
-    ),
-    tabItem("boxplot_menu",
-            "Sub-item 1 tab content"
-    ),
-    tabItem("regression_menu",
-            "Sub-item 2 tab content"
-    ),
-    tabItem("developer_menu",
-            "Sub-item 2 tab content"
-    ),
-    tabItem("macro_menu",
-            "Sub-item 2 tab content"
-    ),
-    tabItem("about_menu",
-            "Sub-item 2 tab content"
-    )
+    tabItem("heatmap_menu", uiOutput("heatmap_ui")),
+    tabItem("diff_heatmap_menu", uiOutput("diff_heatmap_ui")),
+    tabItem("boxplot_menu", uiOutput("boxplot_ui")),
+    tabItem("regression_menu", uiOutput("regression_ui")),
+    tabItem("developer_menu", uiOutput("developer_ui")),
+    tabItem("macro_menu", uiOutput("macro_ui")),
+    tabItem("about_menu", uiOutput("about_ui"))
   )
 )
 
@@ -100,11 +95,98 @@ ui = dashboardPage(
 server = function(input, output, session) {
   
   observeEvent(input$data_file, {
+    output$heatmap_ui = renderUI({
+      fluidRow(
+        column(8, actionBttn(inputId = "plot_heatmap", label = "Plot Heatmap", 
+                             block = TRUE, color = "royal", style = "fill"))
+      )
+    })
+  })
+  
+  observeEvent(input$data_file, {
+    output$diff_heatmap_ui = renderUI({
+      fluidRow(
+        column(8, actionBttn(inputId = "plot_diff_heatmap", label = "Plot Diff Heatmap", 
+                             block = TRUE, color = "success", style = "fill"))
+      )
+    })
+  })
+  
+  observeEvent(input$data_file, {
+    output$boxplot_ui = renderUI({
+      fluidRow(
+        column(8, actionBttn(inputId = "plot_boxplos", label = "Plot Boxplot", 
+                             block = TRUE, color = "primary", style = "fill"))
+      )
+    })
+  })
+  
+  observeEvent(input$data_file, {
+    output$regression_ui = renderUI({
+      fluidRow(
+        column(8, actionBttn(inputId = "plot_regression", label = "Plot Regression Plot", 
+                             block = TRUE, color = "danger", style = "fill"))
+      )
+    })
+  })
+  
+  output$developer_ui = renderUI({
+    initial_value = "
+    ### Use dff to refer to the data frame uploaded
+    list(
+    data_summary = summary(dff)
+    )
+    "
+    box(width = 12, solidHeader = TRUE, title = "Terminal",
+        aceEditor("code", mode = "r", height = "200px", value = initial_value),
+        fluidRow(
+          column(4, actionBttn(inputId = "eval_code", label = "Analyse", 
+                               block = TRUE, style = "fill", color = "royal"))),
+        hr(),
+        verbatimTextOutput("terminal_output")
+    )
+  })
+  
+  output$terminal_output = renderPrint({
+    input$eval_code
+    eval(parse(text = isolate(input$code)))
+  })
+  
+  output$macro_ui = renderUI({
+    init_value = "
+    Fill in the description of the macro uploaded.
+    E.g:
+    \"\"\"
+    This Macro is used to run multiple linear regressions on uploaded data files..
+    Function X and Function Y are used for ...
+    This Macro is composed by WG 2019 and IX 2019 ... 
+    \"\"\"
+    "
+    box(
+      fileInput(inputId = "uploaded_macro", label = "Upload Macro", multiple = TRUE),
+      hr(),
+      h4("Macro Descriotion"),
+      textAreaInput(inputId = "macro_description", label = NULL, placeholder = init_value,
+                    resize = "both", width = "300", height = "300px"),
+      actionBttn(inputId = "submit_macro", label = "Submit Macro", style = "fill",
+                 block = TRUE, color = "primary")
+    )
+  })
+  
+  output$about_ui = renderUI({
+    box(width = 12,
+        includeMarkdown("README.md")
+    )
+  })
+  
+  #####################################################
+  
+  observeEvent(input$data_file, {
     output$header_title = renderUI({
       fluidRow(
-        box(
-          h4(tags$strong(paste0("Uploaded Table: ", input$data_file$name))),
-          h5(paste0("Upload Time: ", as.character(Sys.time())))
+        box(width = 12,
+            h4(tags$strong(paste0("Uploaded Table: ", input$data_file$name))),
+            h5(paste0("Upload Time: ", as.character(Sys.time())))
         )
       )
     })
@@ -114,9 +196,9 @@ server = function(input, output, session) {
     output$show_hide_summary = renderUI({
       div(hr(),
           fluidRow(
-            column(width = 4,actionBttn(inputId = "show_summary_button", label = "Show Data Summary",
+            column(width = 4,actionBttn(inputId = "show_summary_button", label = "Show Table",
                                         style = "fill", block = TRUE, color = "success")),
-            column(width = 4,actionBttn(inputId = "hide_summary_button", label = "Hide Data Summary",
+            column(width = 4,actionBttn(inputId = "hide_summary_button", label = "Hide Table",
                                         style = "fill", block = TRUE, color = "warning"))
           ))
     })
